1bccfb82d7a3b5fa7eafed05cde55d0b5b704564
platform-42 <142492114+platform-42@users.noreply.github.com>
2026-09-09 21:06:35 +0200

    dddd

--- test_gino.py
+++ test_gino.py
@@ -4,5 +4,8 @@ from ops_stats import (
 )
 update_stats("Platform42", "CHANNEL", "WhatsApp", 1000, 25, 312.450)
 update_stats("Platform42", "CHANNEL", "Instagram", 4, 1, 12.450)
-update_state("Platform42", "ORCHESTRATOR", "orchestrator", True)
+update_state("Platform42", "ORCHESTRATOR", "Orchestrator", True)
+
 update_stats("BlueFez", "CHANNEL", "WhatsApp", 1, 1, 100.0)
+update_state("BlueFez", "ORCHESTRATOR", "Orchestrator", False)
+
