c528dabdddd471186b6f90eb76444bdacfe13ed4
platform-42 <142492114+platform-42@users.noreply.github.com>
2026-09-10 09:06:12 +0200

    dddd

--- test_gino.py
+++ test_gino.py
@@ -2,10 +2,17 @@ from ops_stats import (
     update_stats,
     update_state
 )
-update_stats("Platform42", "CHANNEL", "WhatsApp", 1000, 25, 312.450)
-update_stats("Platform42", "CHANNEL", "Instagram", 4, 1, 12.450)
-update_state("Platform42", "ORCHESTRATOR", "Orchestrator", True)
 
+#
+# prerequisite: 
+#   make sure the ops_stats extension is installed in your database
+#   make sure that ops.customers is populated (e.g. BlueFez, Platform42, etc.) 
+#   make sure that osps.components is populated (e.g. WhatsApp, Orchestrator, etc.)
+#
+
+# report that 1 item was processed for BlueFez, with 1 error and an average response time of 100ms
 update_stats("BlueFez", "CHANNEL", "WhatsApp", 1, 1, 100.0)
+
+# report that orchestrator is down for BlueFez
 update_state("BlueFez", "ORCHESTRATOR", "Orchestrator", False)
 
