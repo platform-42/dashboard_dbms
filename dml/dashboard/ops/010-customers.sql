142e8710112bbfa90989224345de83ef96d4aae9
platform-42 <142492114+platform-42@users.noreply.github.com>
2026-09-16 22:09:49 +0200

    cccc

--- dml/dashboard/ops/010-customers.sql
+++ dml/dashboard/ops/010-customers.sql
@@ -1,11 +1,12 @@
 -- Customers
 DELETE FROM ops.customer
     ;
-    
+
 INSERT INTO ops.customer 
     (name)
 VALUES
     ('Platform42'),
     ('BlueFez'),
-    ('InnovativeAISolutions')
+    ('InnovativeAISolutions'),
+    ('Esolutionssystems')
     ;
