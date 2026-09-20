78d26f38bc5dd026ff3684daf0e37718901348bf
platform-42 <142492114+platform-42@users.noreply.github.com>
2026-09-20 23:27:10 +0200

    ddd

--- test_gino.py
+++ test_gino.py
@@ -32,6 +32,10 @@ if __name__ == "__main__":
     update_stats("Platform42", "CHANNEL", "Instagram", random.randint(3, 9), 2, 55.0)
     update_stats("Platform42", "CHANNEL", "WhatsApp", random.randint(11, 20), 3, 180.0)
     time.sleep(20)
+    # bump second round of stats
+    update_stats("Platform42", "CHANNEL", "Instagram", random.randint(21, 41), 6, 55.0)
+    update_stats("Platform42", "CHANNEL", "WhatsApp", random.randint(100, 20), 3, 180.0)
+    time.sleep(20)
     # force an exception to test the uncaught exception handler
     a = 10/0
 
