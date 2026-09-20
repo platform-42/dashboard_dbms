338b6c9d29803b97d55cde5b905d40cf3ca5ae9b
platform-42 <142492114+platform-42@users.noreply.github.com>
2026-09-20 23:25:26 +0200

    ddd

--- test_gino.py
+++ test_gino.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import random
 
 from ops_stats import (
     update_stats,
@@ -19,11 +20,18 @@ def handle_uncaught_exception(exc_type, exc_value, exc_traceback):
 sys.excepthook = handle_uncaught_exception
 
 if __name__ == "__main__":
+    # update states
     update_state("Platform42", "ORCHESTRATOR", "Orchestrator", True)
     update_state("Platform42", "CHANNEL", "WhatsApp", True)
     update_state("Platform42", "CHANNEL", "Instagram", True)
+    # bump first round of stats
     update_stats("Platform42", "CHANNEL", "WhatsApp", 3, 1, 180.0)
     update_stats("Platform42", "CHANNEL", "Instagram", 4, 0, 55.0)
     time.sleep(20)
+    # bump second round of stats
+    update_stats("Platform42", "CHANNEL", "Instagram", random.randint(3, 9), 2, 55.0)
+    update_stats("Platform42", "CHANNEL", "WhatsApp", random.randint(11, 20), 3, 180.0)
+    time.sleep(20)
+    # force an exception to test the uncaught exception handler
     a = 10/0
 
